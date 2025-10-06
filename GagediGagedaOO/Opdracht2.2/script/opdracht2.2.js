720c3326ee01a19f0871a7603e863c8fb8aa2377
Rohaneke <101725@glr.nl>
2025-10-06 14:54:57 +0200

    skibidi

--- GagediGagedaOO/Opdracht2.2/script/opdracht2.2.js
+++ GagediGagedaOO/Opdracht2.2/script/opdracht2.2.js
@@ -3,12 +3,21 @@ const studenten = [
   { "studentnaam": "David", "studentnummer": "12345" }
 ];
 
-const outputDiv = document.getElementById('output');
-let html = '';
+const outputDiv = document.getElementById('personen');
+let kanker = '';
 
 for (const student of studenten) {
-  html += `Student Naam: ${student.studentnaam}<br>`;
-  html += `Student Nummer: ${student.studentnummer}<br><br>`;
+  let tr = document.createElement('tr');
+  let tdNaam = document.createElement('td');
+  let tdNummer = document.createElement('td');
+
+  tdNaam.textContent = student.studentnaam;
+  tdNummer.textContent = student.studentnummer;
+
+  tr.appendChild(tdNaam);
+  tr.appendChild(tdNummer);
+
+  kanker += tr.outerHTML;
 }
 
-outputDiv.innerHTML = html;
+outputDiv.innerHTML = kanker;
